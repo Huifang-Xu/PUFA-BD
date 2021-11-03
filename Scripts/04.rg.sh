f5c4007273c83acc19045324372bc34aa1b8825d
Huifang-Xu <91753987+Huifang-Xu@users.noreply.github.com>
2021-11-03 15:58:08 -0400

    Update 04.rg.sh

--- Scripts/04.rg.sh
+++ Scripts/04.rg.sh
@@ -6,7 +6,7 @@
 source activate ldsc 
 
 /home/hx37930/ldsc/ldsc.py \
-    --rg scz.sumstats.gz,bip.sumstats.gz \ #input munged data of 2 phenotypes
+    --rg met-d-Omega_3.convertP.munge.sumstats.gz,met-d-Omega_6.convertP.munge.sumstats.gz \ #input munged data of 2 phenotypes
     --ref-ld-chr eur_w_ld_chr/ \ #file of LD score for 22 chromosomes
     --w-ld-chr eur_w_ld_chr/ \  #file of LD score weight for 22 chromosomes
-    --out scz_vs_bip   #Output file prefix
+    --out Omega_3_vs_Omega_6   #Output file prefix
