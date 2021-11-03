e6f5a7561d625669958628f8ee978fcfd6b802b5
Huifang-Xu <91753987+Huifang-Xu@users.noreply.github.com>
2021-11-03 15:39:44 -0400

    Update 03.munge.sh

--- Scripts/03.munge.sh
+++ Scripts/03.munge.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Munge data - GWAS summary statistics
+
 #activate ldsc environment
 source activate ldsc 
 
 #required input columns of the GWAS summary statistics include: SNP ID, chrom, BP, A1, A2, beta/OR/Z, P value
 
-#Step1: munge data - GWAS summary statistics
 #munge data for Phenotype 1
 /home/hx37930/ldsc/munge_sumstats.py \
     --sumstats scz.swe.pgc1.results.v3.txt \ #GWAS summary statistics data of Phenotype 1
@@ -19,10 +20,3 @@ source activate ldsc
     --N 11810 \ #sample size of phenotype 2
     --out bip \ #Output file prefix
     --merge-alleles w_hm3.snplist #Hapmap3 reference panel (contain well-imoutated SNPs)
-
-#Step2: Conduct LDSC. Estimating Heritability, Genetic Correlation and the LD Score Regression Intercept.
-/home/hx37930/ldsc/ldsc.py \
-    --rg scz.sumstats.gz,bip.sumstats.gz \ #input munged data of 2 phenotypes
-    --ref-ld-chr eur_w_ld_chr/ \ #file of LD score for 22 chromosomes
-    --w-ld-chr eur_w_ld_chr/ \  #file of LD score weight for 22 chromosomes
-    --out scz_vs_bip   #Output file prefix
