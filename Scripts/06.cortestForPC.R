84c55da233459c28026f9c5386f48762aa0ac733
Huifang-Xu <91753987+Huifang-Xu@users.noreply.github.com>
2021-11-04 03:16:38 +0800

    Create 06.cortestForPC.R

---
+++ Scripts/06.cortestForPC.R
@@ -0,0 +1,28 @@
+#!/bin/Rscript
+
+#This script will do cor.test to estimate phenotypic correlation among 14 fatty acids.
+
+#load package
+library(tidyverse)
+
+#read phenotypic data
+df <- read.table(file="phenotype.txt",sep="\t")
+#data format of phenotype.txt:
+  #SampleID Phenotype1 Phenotype2 Phenotype3
+  #ID1  1 2 3
+
+#build a empty tibble to store result
+corr_results <- tibble(p1="phenotype1",p2="phenotype2",rp=0)
+
+#do correlation
+for (i in 2:ncol(df)){
+  for (j in 2:ncol(df)){
+    corr <- cor(df[,i],df[,j],method="pearson")
+    corr_results <- add_row(corr_results,p1=colnames(df[,i]),p2=colnames(df[,j]),rp=corr)
+    }
+}
+
+#transfer format to matrix
+pc_matrix <- corr_results[-1,]
+pc_matrix <- acast(pc_matrix,p1~p2)
+pc_matrix <- as.matrix(pc_matrix)
