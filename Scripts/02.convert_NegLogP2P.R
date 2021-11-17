83e124dbb4b07ff15de60a61b3bf796b732dcdee
Huifang-Xu <91753987+Huifang-Xu@users.noreply.github.com>
2021-11-17 10:18:58 -0500

    Update 02.convert_NegLogP2P.R

--- Scripts/02.convert_NegLogP2P.R
+++ Scripts/02.convert_NegLogP2P.R
@@ -6,11 +6,11 @@
 Args <- commandArgs()
 
 #read table
-df <- read.table(file=Args[1],header=T,sep="\t")
+df <- read.table(file=Args[6],header=T,sep="\t")
 
 #convert -log10 P to P, add a new column to store P value
 P <- 1:nrow(df)
 for (i in 1:nrow(df)){P[i] <- 1/(10^df[i,9])}
 
 #write result
-write.table(cbind(df,P),file=Args[2],quote=FALSE,sep="\t",row.names=FALSE,col.names=TRUE)
+write.table(cbind(df,P),file=Args[7],quote=FALSE,sep="\t",row.names=FALSE,col.names=TRUE)
