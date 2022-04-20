838c8c375f25f3ab861ef6a922da902ba189f3a2
Huifang-Xu <91753987+Huifang-Xu@users.noreply.github.com>
2022-04-20 15:07:03 -0400

    Create extract_LDSC_result.sh

---
+++ Scripts/extract_LDSC_result.sh
@@ -0,0 +1,15 @@
+#! bin/bash
+
+# This script aims to extract summary lines from each pair-wise LDSC result file
+
+#write header
+sed -rn '61p' AD/AD_30617256_vs_CHARGE_DHA.log  > all.LDSC.pair.tsv
+
+#all pair-wise LDSC result files
+ls */*.log > log.list
+
+#extract summary lines from each pair-wise LDSC result file
+for i in `cat log.list`
+do
+  tail -n 5 $i | grep "scratch" >> all.LDSC.pair.tsv
+done
