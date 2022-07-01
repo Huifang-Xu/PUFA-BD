f147c156f19c9f96d8bd56f109ab7f176b0ff0f3
Huifang-Xu <91753987+Huifang-Xu@users.noreply.github.com>
2022-07-01 15:44:06 -0400

    Update 01.convertVCF2TXT.sh

--- Scripts/01.convertVCF2TXT.sh
+++ Scripts/01.convertVCF2TXT.sh
@@ -6,8 +6,8 @@
 
 #convert GWAS summary statistics vcf to txt.
 zcat met-d-Omega_3.vcf.gz \
-  | awk 'BEGIN{FS=OFS="\t"}$1!~/#/{if($3~/rs/) {print $3,$1,$2,$4,$5,$10} else {print "chr"$1"_"$2"_"$4"_"$5,$1,$2,$4,$5,$10}}' \
-  | awk -v OFS="\t" 'BEGIN{FS=":";print "SNP\tCHR\tBP\tA1\tA2\tbeta\tSE\tAF\tlP"}{print $1,$2,$4,$3}' > met-d-Omega_3.convert.txt
+  | awk 'BEGIN{FS=OFS="\t"}$1!~/#/{print $3,$1,$2,$4,$5,$10}' \
+  | awk -v OFS="\t" 'BEGIN{FS=":";print "SNP\tCHR\tBP\tA1\tA2\tBETA\tSE\tEAF\tLP"}{print $1,$2,$4,$3}' > met-d-Omega_3.convert.txt
 
 #convert -log10 P to P value. But it is recommended to convert -log 10 P using R script because there are limitations using AWK
 #awk -v FS="\t" 'NR>1{printf "%s\t%s\t%s\t%s\t%s\t%8e\t%.8f\t%.8f\t%.8f\t%.8f\n",$1,$2,$3,$4,$5,$6,$7,$8,$9,strtonum(1/(10^$9))}' met-d-Omega_3.convert.txt |awk 'BEGIN{FS="\t";print "SNP\tCHR\tBP\tA1\tA2\tbeta\tSE\tAF\tlP\tP"}{print $0}' > met-d-Omega_3.convertP.txt
